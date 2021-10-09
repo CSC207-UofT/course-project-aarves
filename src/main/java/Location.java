e9c47191d852c472a9d3c593313582cfd748a44f
vt111 <vaishnavit369@gmail.com>
2021-10-09 15:50:08 -0400

    partially implemented location

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,2 +1,20 @@
-public class Location {
+import java.util.List;
+
+public abstract class Location {
+    private String hours_of_service;
+    private List<Review> reviews;
+    private String area;
+
+    public Location(List<Review> reviews, String hours_of_service){
+
+    }
+
+    public String getHours_of_service() {
+        return hours_of_service;
+    }
+
+    public int get_avgrating(){
+     return 0;
+    }
+
 }
