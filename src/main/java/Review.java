e1996b460e0c57d312ee81c19f72413dadfbe462
ashenafee <ashenafeem@gmail.com>
2021-10-09 15:35:43 -0400

    Added two constructors for Review (one with detailed review, one without), getters for attributes, and a toString() method.

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -1,2 +1,35 @@
 public class Review {
+    private User reviewer;
+    private String review;
+    private int rating;
+
+    public Review(User reviewer, String review, int rating) {
+        this.reviewer = reviewer;
+        this.review = review;
+        this.rating = rating;
+    }
+
+    public Review(User reviewer, int rating) {
+        this.reviewer = reviewer;
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Rating:\t%s/%s\n" +
+                             "Review:\t%s", this.rating, 5, this.review);
+    }
+
+    public static void main(String[] args) {
+        Review test = new Review(new User(), "Fantastic place!", 4);
+        System.out.println(test);
+    }
 }
