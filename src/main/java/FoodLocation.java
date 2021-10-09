f09fd457001f9843fd942fe4b6b19f6c393f7ebf
vt111 <vaishnavit369@gmail.com>
2021-10-09 16:27:19 -0400

    made changes to location, foodlocation, and studylocation

--- src/main/java/FoodLocation.java
+++ src/main/java/FoodLocation.java
@@ -1,2 +1,40 @@
-public class FoodLocation {
+import java.util.List;
+
+public class FoodLocation extends Location{
+    private String foodtype;
+    private boolean takeout;
+    private boolean dinein;
+    private int pricerange;
+
+    public FoodLocation(List<Review> reviews, String hours_of_service, String area,
+                        String food, boolean takeout, boolean dinein, int pricerange) {
+        super(reviews, hours_of_service, area);
+        this.foodtype = food;
+        this.takeout = takeout;
+        this.dinein = dinein;
+        this.pricerange = pricerange;
+    }
+
+    public boolean isDinein() {
+        return dinein;
+    }
+
+    public boolean isTakeout() {
+        return takeout;
+    }
+
+    public String getFoodtype() {
+        return foodtype;
+    }
+
+    public int getPricerange() {
+        return pricerange;
+    }
+
+    @Override
+    public String toString() {
+        return (super.getName()+"\n Address: "+super.getArea()+"\n Hours of Service: "+super.getHours_of_service()
+                +"\n Rating: " +super.get_avgrating()+"\n"+getFoodtype()+"\n dine-in: "+isDinein()+"\n takeout: "+isTakeout()
+                +"\n"+getPricerange());
+    }
 }
--- src/main/java/StudyLocation.java
+++ src/main/java/StudyLocation.java
@@ -1,2 +1,8 @@
-public class StudyLocation {
+import java.util.List;
+
+public class StudyLocation extends Location{
+    
+    public StudyLocation(List<Review> reviews, String hours_of_service, String area) {
+        super(reviews, hours_of_service, area);
+    }
 }
