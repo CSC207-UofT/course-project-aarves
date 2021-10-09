cade4da5aef4d08219d0fc03022700b47afedd9c
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-10-09 15:01:01 -0400

    Testing Github autograding using ExampleTest.

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,27 @@
+/*
+ * This file contains JUnit test cases for BagMainTest.java and the
+ * methods from the other Bag classes.
+ *
+ * You should NOT modify this file! When you push Basics.java to
+ * GitHub, it should run these tests. You can see the results in the
+ * "Actions" tab of your repository.
+ */
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleTest {
+    AccountCreator ac;
+
+    @Before
+    public void setUp() throws Exception {
+        ac = new AccountCreator();
+    }
+
+    @Test(timeout = 50)
+    public void TestEnhanceBagsTrue() {
+        assertTrue(ac instanceof AccountCreator);
+    }
+}
