d0ea649be0acf9c87f91cabee1e9d4cd4fd4f42f
vt111 <vaishnavit369@gmail.com>
2021-10-09 15:56:54 -0400

    filled in constructor

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -4,9 +4,11 @@ public abstract class Location {
     private String hours_of_service;
     private List<Review> reviews;
     private String area;
+    //area grabbed from maps
 
     public Location(List<Review> reviews, String hours_of_service){
-
+        this.reviews = reviews;
+        this.hours_of_service = hours_of_service;
     }
 
     public String getHours_of_service() {
