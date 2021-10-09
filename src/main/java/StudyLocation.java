2d880edf527d9969b07a3d237516eea44151f4b8
vt111 <vaishnavit369@gmail.com>
2021-10-09 16:34:10 -0400

    Update StudyLocation.java

--- src/main/java/StudyLocation.java
+++ src/main/java/StudyLocation.java
@@ -1,8 +1,26 @@
 import java.util.List;
 
 public class StudyLocation extends Location{
-    
-    public StudyLocation(List<Review> reviews, String hours_of_service, String area) {
+    private boolean indoors;
+    private boolean quiet;
+    public StudyLocation(List<Review> reviews, String hours_of_service, String area, boolean indoors,
+                         boolean quiet) {
         super(reviews, hours_of_service, area);
+        this.indoors = indoors;
+        this.quiet = quiet;
+    }
+
+    public boolean isIndoors() {
+        return indoors;
+    }
+
+    public boolean isQuiet() {
+        return quiet;
+    }
+
+    @Override
+    public String toString() {
+        return (super.getName()+"\n Address: "+super.getArea()+"\n Hours of Service: "+super.getHours_of_service()+
+                "\n Rating: " +super.get_avgrating() + "\n Indoors:"+isIndoors()+"\n Quiet: "+isQuiet());
     }
 }
