85fadcc3a05c45801ebde44a63102713d3bccde9
Anthony Louie <7217219+lanthony42@users.noreply.github.com>
2021-10-14 15:17:21 -0400

    Update FoodLocation.java

--- src/main/java/FoodLocation.java
+++ src/main/java/FoodLocation.java
@@ -1,2 +1,41 @@
-public class FoodLocation {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodLocation extends Location{
+    private String foodtype;
+    private boolean takeout;
+    private boolean dinein;
+    private String pricerange;
+
+    public FoodLocation(String hours_of_service, String area, String food,
+                        boolean takeout, boolean dinein, String pricerange) {
+        super(hours_of_service, area);
+        this.foodtype = food;
+        this.takeout = takeout;
+        this.dinein = dinein;
+        this.pricerange = pricerange;
+    }
+
+    public boolean isDinein() {
+        return dinein;
+    }
+
+    public boolean isTakeout() {
+        return takeout;
+    }
+
+    public String getFoodtype() {
+        return foodtype;
+    }
+
+    public String getPricerange() {
+        return pricerange;
+    }
+
+    @Override
+    public String toString() {
+        return (super.getName()+"\n Address: "+super.getArea()+"\n Hours of Service: "+super.getHours_of_service()
+                +"\n Rating: " +super.get_avgrating()+"\n"+getFoodtype()+"\n dine-in: "+isDinein()+"\n takeout: "+isTakeout()
+                +"\n"+getPricerange());
+    }
 }
