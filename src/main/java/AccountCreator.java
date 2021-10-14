73ddf3451f0893c9b104ed6bd660ca10377062a7
erica27 <erica.eng@mail.utoronto.ca>
2021-10-14 14:59:17 -0400

    Added documentation to AccountCreator

--- src/main/java/AccountCreator.java
+++ src/main/java/AccountCreator.java
@@ -1,5 +1,13 @@
 public class AccountCreator {
 
+    /**
+     * Checks if the given account already exists, if not a new account will be created
+     *
+     * @param manager An AccountManager
+     * @param username A User's username
+     * @param password A User's password
+     * @return true if a new account has been created, false otherwise
+     */
     public boolean createAccount(AccountManager manager, String username, String password) {
         if (manager.isExistingAccount(username)) {
             return false;
