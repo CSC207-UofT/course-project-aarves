eb5df0fbb0496849a6d538d0140146793bb2e1e9
oNatsyy <natiqali85@gmail.com>
2021-10-14 14:24:15 -0400

    added formal documentation

--- src/main/java/GuestUser.java
+++ src/main/java/GuestUser.java
@@ -1,2 +1,10 @@
-public class GuestUser {
+/**
+ * Child class of User and has no attributes.
+ *
+ * This class does not take any parameters.
+ */
+
+public class GuestUser extends User{
+
+    public GuestUser(){}
 }
