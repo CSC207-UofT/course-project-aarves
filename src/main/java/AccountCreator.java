818d7250af0128ca66b6f96da0c121db209f316e
oNatsyy <natiqali85@gmail.com>
2021-10-14 14:55:07 -0400

    added formal documentation

--- src/main/java/AccountCreator.java
+++ src/main/java/AccountCreator.java
@@ -1,12 +1,2 @@
 public class AccountCreator {
-
-    public boolean createAccount(AccountManager manager, String username, String password) {
-        if (manager.isExistingAccount(username)) {
-            return false;
-        }
-        else {
-            manager.addUser(username, password);
-            return true;
-        }
-    }
 }
--- src/main/java/ReviewManager.java
+++ src/main/java/ReviewManager.java
@@ -1,74 +1,17 @@
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public class ReviewManager {
-
     private static HashMap<Integer, Review> reviewHashMap = new HashMap<>();
+
+    protected boolean isIdUnique(int reviewId) {
+        return !reviewHashMap.containsValue(reviewId);
+    }
 
     protected Review getReview(int reviewId) {
         return reviewHashMap.get(reviewId);
     }
 
-    /**
-     * Creates a new review and adds it to the reviewHashMap
-     *
-     * @param reviewer the Review's RegisteredUser
-     * @param location the Review's Location
-     * @param review the Review's text
-     * @param rating the Review's rating
-     */
-    public void createReview(RegisteredUser reviewer, Location location, String review, int rating) {
-        Review new_review = new Review(reviewer, location, review, rating);
-        addReview(location, new_review);
-    }
-
-    /**
-     * Creates a new review and adds it to the reviewHashMap.
-     *
-     * @param reviewer the Review's RegisteredUser
-     * @param location the Review's Location
-     * @param rating the Review's rating
-     */
-    public void createReview(RegisteredUser reviewer, Location location, int rating) {
-        Review new_review = new Review(reviewer, location, rating);
-        addReview(location, new_review);
-    }
-
-    /**
-     * Adds a review to reviewHashMap.
-     *
-     * @param location the Location of the review
-     * @param review the Review to be added
-     */
-    protected void addReview(Location location, Review review) {
+    protected void addReview(Review review) {
         reviewHashMap.put(review.getReviewId(), review);
-        review.getReviewer().addReview(review);
-        location.addReview(review);
     }
-
-    /**
-     * Deletes all reviews associated with a RegisteredUser.
-     *
-     * @param reviewer the RegisteredUser whose reviews will be deleted.
-     */
-    public void deleteAllUserReviews(RegisteredUser reviewer) {
-        ArrayList<Review> reviews = reviewer.getReviews();
-        for (Review r : reviews) {
-            deleteReview(reviewer, r.getLocation(), r);
-        }
-    }
-
-    /**
-     * Deletes a review from all three locations where it is stored.
-     *
-     * @param reviewer the RegisteredUser of the review
-     * @param location the Location of the review
-     * @param review the Review to be deleted
-     */
-    public void deleteReview(RegisteredUser reviewer, Location location, Review review) {
-        reviewHashMap.remove(review.getReviewId());
-        reviewer.deleteReview(review);
-        location.deleteReview(review);
-    }
-
 }
