d9b95eeb9b13d984a162790d97ac8ea18d27f66b
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-11-13 15:19:12 -0500

    Properly refactored AccountManagerTest.java

--- core/src/test/java/AccountManagerTest.java
+++ core/src/test/java/AccountManagerTest.java
@@ -1,3 +1,6 @@
+import com.aarves.core.usecases.AccountManager;
+import com.aarves.core.usecases.ReviewManager;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
