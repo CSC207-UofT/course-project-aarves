d3f7616ac2cef6c8a3cfaff562f917e0a95db09b
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-11-21 14:20:54 -0500

    Set up basics for AccountController.

--- core/src/main/java/com/aarves/bluepages/adapters/AccountController.java
+++ core/src/main/java/com/aarves/bluepages/adapters/AccountController.java
@@ -8,4 +8,28 @@ public class AccountController {
     public AccountController(AccountManager accountManager) {
         this.accountManager = accountManager;
     }
+
+    public void login(String username, String password) {
+        if(this.accountManager.isLoggedIn()) {
+            this.accountManager.logout();
+        }
+
+        boolean result = this.accountManager.login(username, password);
+
+        // TODO: Some presenter stuff
+    }
+
+    public void register(String username, String password, String confirmPassword) {
+        if(this.accountManager.isLoggedIn()) {
+            this.accountManager.logout();
+        }
+
+        // TODO: Some presenter stuff
+        if(password.equals(confirmPassword)) {
+            this.accountManager.register(username, password);
+        }
+        else {
+
+        }
+    }
 }
