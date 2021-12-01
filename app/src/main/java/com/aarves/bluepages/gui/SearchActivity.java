bd101787b234bd39468908fcdedd1726be4050c9
vt111 <vaishnavit369@gmail.com>
2021-11-30 20:45:05 -0500

    created respective java file for search/filter

---
+++ app/src/main/java/com/aarves/bluepages/gui/SearchActivity.java
@@ -0,0 +1,54 @@
+package com.aarves.bluepages.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SearchView;
+import java.util.Collections;
+
+import com.aarves.bluepages.R;
+import com.aarves.bluepages.adapter.controllers.LookupController;
+import com.aarves.bluepages.adapter.MapboxGateway;
+import com.aarves.bluepages.entities.Location;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+public class SearchActivity extends AppCompatActivity {
+
+
+    private Button foodButton, locationButton, allButton;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+//    private void unSelectAllFilterButtons()
+//    {
+//        lookUnSelected(allButton);
+//        lookUnSelected(foodButton);
+//        lookUnSelected(locationButton);
+//    }
+//    private void lookSelected(Button parsedButton)
+//    {
+//        parsedButton.setTextColor();
+//        parsedButton.setBackgroundColor();
+//    }
+//
+//    private void lookUnSelected(Button parsedButton)
+//    {
+//        parsedButton.setTextColor();
+//        parsedButton.setBackgroundColor();
+//    }
+}
