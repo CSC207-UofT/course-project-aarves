8d315f3cd90596fce4b2dfbd811ad500d1b7afc2
oNatsyy <natiqali85@gmail.com>
2021-12-04 01:01:02 -0500

    Added minor changes to ToString() in Study/Food Location.java

--- core/src/main/java/com/aarves/bluepages/entities/FoodLocation.java
+++ core/src/main/java/com/aarves/bluepages/entities/FoodLocation.java
@@ -69,7 +69,7 @@ public class FoodLocation extends Location {
     @Override
     public String toString() {
         return (super.getName() + "\n Address: " + super.getAddress() + "\n Hours of Service: "
-                + super.getHoursOfService() + "\n" + getFoodType() + "\n dine-in: " + isDineIn()
-                + "\n takeout: " + isTakeout() + "\n" + getPriceRange());
+                + super.getHoursOfService() + "\n Food Type: " + getFoodType() + "\n dine-in: " + isDineIn()
+                + "\n takeout: " + isTakeout() + "\n Price Range: " + getPriceRange());
     }
 }
