9504f0bdce5ccdaa59271f13973f5d03846311d8
oNatsyy <natiqali85@gmail.com>
2021-12-05 20:38:38 -0500

    added file for AccountDataAccess testing

---
+++ core/src/test/java/com/aarves/bluepages/usecase/data/AccountDataAccessTest.java
@@ -0,0 +1,49 @@
+package com.aarves.bluepages.usecase.data;
+
+import com.aarves.bluepages.entities.User;
+import com.aarves.bluepages.usecase.interactors.review.ReviewRepositoryMockup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDataAccessTest {
+
+    AccountDataAccess ada;
+    AccountDAO ao;
+    ReviewRepositoryMockup rpm;
+
+    @BeforeEach
+    void setUp() {
+        ada = new AccountDataAccess(ao, rpm);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addAccount() {
+        User new_user = new User("Natsyy", "M$a%tB1rya*i");
+        ada.addAccount(new_user);
+
+        assertEquals();
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+
+    @Test
+    void getUserAccount() {
+    }
+
+    @Test
+    void isExistingAccount() {
+    }
+
+    @Test
+    void isPasswordMatch() {
+    }
+}
