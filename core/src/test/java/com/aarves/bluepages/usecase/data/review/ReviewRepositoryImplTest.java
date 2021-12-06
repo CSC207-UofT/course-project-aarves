1c545e2fbdf7c3c48f21f493a943e292f192426b
erica27 <erica.eng@mail.utoronto.ca>
2021-12-06 12:33:14 -0500

    Testing so far for ReviewRepositoryImpl

--- core/src/test/java/com/aarves/bluepages/usecase/data/review/ReviewRepositoryImplTest.java
+++ core/src/test/java/com/aarves/bluepages/usecase/data/review/ReviewRepositoryImplTest.java
@@ -8,16 +8,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReviewRepositoryImplTest {
 
     ReviewRepositoryImpl reviewRepository;
-    // ReviewDAOMockup reviewDAO;
+    ReviewDAOMock reviewDAO;
 
     @BeforeEach
     void setUp() {
-        // reviewRepository = new ReviewRepositoryImpl(reviewDAO);
+        reviewDAO = new ReviewDAOMock();
+        reviewRepository = new ReviewRepositoryImpl(reviewDAO);
     }
 
     @AfterEach
@@ -26,21 +30,48 @@ class ReviewRepositoryImplTest {
 
     @Test
     void testAddReview() {
+        int original = reviewDAO.reviews.size();
         Review r = new Review("reviewer", 34, 1);
-        Review result = reviewRepository.addReview(r);
-        Assertions.assertEquals(r, result);
+        reviewRepository.addReview(r);
+        Assertions.assertEquals(original + 1, reviewDAO.reviews.size());
     }
 
+    // TODO: concurrent modification error
     @Test
-    void deleteReview() {
+    void testDeleteReview() {
+        Review r = new Review("reviewer", 34, 1);
+        reviewRepository.addReview(r);
+        int size = reviewDAO.reviews.size();
+        reviewRepository.deleteReview(r);
+        size = size - 1;
+        assertEquals(size, reviewDAO.reviews.size());
+
     }
 
+    // TODO: not passing
     @Test
     void getReview() {
+        Review r = new Review(0,"reviewer", 34, 1);
+        Review r2 = new Review(99, "reviewer", 34, 1);
+        reviewRepository.addReview(r);
+        reviewRepository.addReview(r2);
+        Review result = reviewRepository.getReview(0);
+        assertEquals(r, result);
+
     }
 
+    // TODO: not passing
     @Test
     void getReviewsByUser() {
+        Review r = new Review(0,"reviewer", 34, 1);
+        Review r2 = new Review(99, "reviewer", 101, 5);
+        reviewRepository.addReview(r);
+        reviewRepository.addReview(r2);
+        List<Review> result = reviewRepository.getReviewsByUser("reviewer");
+        List<Review> reviewList = new ArrayList<>();
+        reviewList.add(r);
+        reviewList.add(r2);
+        assertEquals(reviewList, result);
     }
 
     @Test
