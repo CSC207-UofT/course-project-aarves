145e484d6b652a8eb8d9c2d8d2fbe1d6d6f97a3d
oNatsyy <natiqali85@gmail.com>
2021-12-06 21:32:38 -0500

    fixed testing for Review adapters

--- core/src/test/java/com/aarves/bluepages/adapter/presenters/ReviewViewMockup.java
+++ core/src/test/java/com/aarves/bluepages/adapter/presenters/ReviewViewMockup.java
@@ -4,20 +4,34 @@ import java.util.List;
 
 public class ReviewViewMockup implements ReviewView{
 
-    boolean reviewDisplayed;
+    private boolean reviewDisplayed;
+    private boolean finishActivity;
+    private String popUpDisplay;
 
     @Override
     public void displayReviews(List<ReviewViewModel> reviews) {
-        reviewDisplayed = true;
+        this.reviewDisplayed = true;
     }
 
     @Override
     public void displayPopUp(String message) {
-
+        this.popUpDisplay = message;
     }
 
     @Override
     public void finishActivity() {
+        this.finishActivity = true;
+    }
 
+    public boolean isReviewDisplayed() {
+        return this.reviewDisplayed;
+    }
+
+    public String getPopUpDisplay(){
+        return this.popUpDisplay;
+    }
+
+    public boolean isFinishActivity() {
+        return this.finishActivity;
     }
 }
