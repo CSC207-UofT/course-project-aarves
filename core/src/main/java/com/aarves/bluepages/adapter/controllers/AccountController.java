86d09eb5421ff5a10102a18a81a338c48f5d43a3
rebplane <rebecca_nhan@rogers.com>
2021-12-08 15:16:04 -0500

    Documentation for controllers and presenters

--- core/src/main/java/com/aarves/bluepages/adapter/controllers/AccountController.java
+++ core/src/main/java/com/aarves/bluepages/adapter/controllers/AccountController.java
@@ -5,10 +5,20 @@ import com.aarves.bluepages.usecase.interactors.account.AccountInputBoundary;
 public class AccountController {
     private final AccountInputBoundary accountInput;
 
+    /**
+     * Constructs a new AccountController object
+     * @param accountInput the AccountInputBoundary interface to inject
+     */
     public AccountController(AccountInputBoundary accountInput) {
         this.accountInput = accountInput;
     }
 
+    /** Logs the user into the account if they are not already logged in. If they are already logged in, log the
+     * user out.
+     *
+     * @param username the username of the user
+     * @param password the password of the user
+     */
     public void login(String username, String password) {
         if(this.accountInput.isLoggedIn()) {
             this.accountInput.logout();
@@ -17,14 +27,28 @@ public class AccountController {
         this.accountInput.login(username, password);
     }
 
+    /**
+     *
+     * @return returns whether the user is currently logged in.
+     */
     public boolean isLoggedIn() {
         return this.accountInput.isLoggedIn();
     }
 
+    /**
+     * Logs the user out of the program.
+     */
     public void logout() {
         this.accountInput.logout();
     }
 
+    /**
+     * Registers the new user's username and password into Bluepages.
+     *
+     * @param username the username of the user
+     * @param password the password to be associated with the username
+     * @param confirmPassword the password again, to ensure that both password fields match
+     */
     public void register(String username, String password, String confirmPassword) {
         if(this.accountInput.isLoggedIn()) {
             this.accountInput.logout();
@@ -33,6 +57,9 @@ public class AccountController {
         this.accountInput.register(username, password, confirmPassword);
     }
 
+    /**
+     * Loads the account information associated with the user for the presenter to present.
+     */
     public void loadAccountInformation() {
         this.accountInput.loadInformation();
     }
--- core/src/main/java/com/aarves/bluepages/adapter/presenters/ReviewViewModel.java
+++ core/src/main/java/com/aarves/bluepages/adapter/presenters/ReviewViewModel.java
@@ -5,30 +5,46 @@ public class ReviewViewModel {
     private final int rating;
     private final String body;
 
+    /**
+     * Creates a ReviewViewModel, containing the review's writer, rating and text of the review.
+     * @param reviewer the reviewer for the review
+     * @param rating the rating of the review
+     * @param body the text body of the review
+     */
     public ReviewViewModel(String reviewer, int rating, String body) {
         this.reviewer = reviewer;
         this.rating = rating;
         this.body = body;
     }
 
-    public ReviewViewModel(String reviewer, int rating) {
-        this.reviewer = reviewer;
-        this.rating = rating;
-        this.body = "";
-    }
-
+    /**
+     * Gets the user who wrote the review.
+     * @return the username of the user who wrote the review.
+     */
     public String getReviewer() {
         return this.reviewer;
     }
 
+    /**
+     * Gets the rating of the review given by the user.
+     * @return the rating of the review.
+     */
     public int getRating() {
         return this.rating;
     }
 
+    /**
+     * Gets the text body of the review given by the user.
+     * @return the text body of the review.
+     */
     public String getBody() {
         return this.body;
     }
 
+    /**
+     * Returns whether the text body of the review is empty or not.
+     * @return whether there is text in the review.
+     */
     public boolean showBody() {
         return !this.body.isEmpty();
     }
