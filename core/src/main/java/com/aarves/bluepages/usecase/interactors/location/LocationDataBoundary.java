a63148cd7803b4805ab1b219dd1f5b7b4a7786e6
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2021-12-08 19:37:45 -0500

    Refactored data classes for review and other misc, added documentation where needed.

--- core/src/main/java/com/aarves/bluepages/usecase/interactors/location/LocationDataBoundary.java
+++ core/src/main/java/com/aarves/bluepages/usecase/interactors/location/LocationDataBoundary.java
@@ -5,10 +5,34 @@ import com.aarves.bluepages.entities.Location;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Interface which allows location use case interactors to access to location data.
+ */
 public interface LocationDataBoundary {
+    /**
+     * Adds given location entity to data source.
+     * @param location the location entity to be added
+     * @return the added location, along with its ID
+     */
     Location addLocation(Location location);
+
+    /**
+     * Deletes given location given ID from data source.
+     * @param locationId the ID of the location to be deleted
+     */
     void deleteLocation(int locationId);
 
+    /**
+     * Retrieves location with given ID from data source.
+     * @param locationId the ID of the location to be retrieved
+     * @return the location entity with given ID, or null if location does not exist
+     */
     Location getLocation(int locationId);
+
+    /**
+     * Retrieves mapping of coordinates to location IDs.
+     * The key will be a list of size 2 with the first element being longitude and the second being latitude.
+     * @return a mapping of coordinates to location IDs
+     */
     Map<List<Long>, Integer> getCoordinatesMap();
 }
--- core/src/main/java/com/aarves/bluepages/usecase/interactors/review/ReviewRepository.java
+++ core/src/main/java/com/aarves/bluepages/usecase/interactors/review/ReviewRepository.java
@@ -4,11 +4,42 @@ import com.aarves.bluepages.entities.Review;
 
 import java.util.List;
 
+/**
+ * Interface for the review repository which allowed for a collection-like interface of review entities by
+ * the repository pattern.
+ */
 public interface ReviewRepository {
+    /**
+     * Adds review to data source.
+     * @param review the review entity to be added
+     * @return the added review, along with its ID
+     */
     Review addReview(Review review);
+
+    /**
+     * Deletes review from data source.
+     * @param review the review entity to be deleted
+     */
     void deleteReview(Review review);
 
+    /**
+     * Retrieves review entity with given ID.
+     * @param reviewId the ID of the review to be retrieved
+     * @return the retrieved review entity
+     */
     Review getReview(int reviewId);
+
+    /**
+     * Retrieves all review entities for a given user.
+     * @param username the username of the user
+     * @return a list of review entities made by the user
+     */
     List<Review> getReviewsByUser(String username);
+
+    /**
+     * Retrieves all review entities for a given location.
+     * @param locationId the ID of the location
+     * @return a list of review entities for the location
+     */
     List<Review> getReviewsByLocation(int locationId);
 }
